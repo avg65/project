881861ccab8c810203405e660cb0d415df2aad19
avg65 <avg6.5ooad@gmail.com>
2013-11-03 22:01:48 +0900

    ss
    ss

---
+++ study/example/SimpleBean.java
@@ -0,0 +1,12 @@
+package.study.simpleBean;
+public class SimpleBean {
+	private String msg;
+	
+	public String getMsg() {
+		return msg;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
